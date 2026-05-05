54cfb5c18a53b5fba769222c4fe8d75e8d8db4f5
FabReady <deerajkumar2067@gmail.com>
2026-05-04 21:48:34 -0400

    Enable Tailwind styling for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://FabReady.github.io',
   base: '/dfma-buddy-site',
+  integrations: [tailwind()],
 });
